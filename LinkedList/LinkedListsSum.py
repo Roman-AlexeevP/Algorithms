c2825fc94639c6c86df02bb53ffee87817fb4143
Roman <alexeevroma@gmail.com>
2022-04-09 12:44:05 +0300

    remove func fo compariton

--- LinkedList/LinkedListsSum.py
+++ LinkedList/LinkedListsSum.py
@@ -1,13 +1,13 @@
 from LinkedList import Node, LinkedList
 
-def compare_lists(first_list: LinkedList, second_list:LinkedList) -> bool:
-    return first_list.len() == second_list.len()
 
 def sum_two_list(first_list: LinkedList, second_list:LinkedList) -> LinkedList:
-    if compare_lists(first_list, second_list):
+    is_equal = first_list.len() == second_list.len()
+    if is_equal:
         node_first_list = first_list.head
         node_second_list = second_list.head
         result_list = LinkedList()
+
         while node_first_list is not None and node_second_list is not None:
             new_node = Node(node_second_list.value+node_first_list.value)
             result_list.add_in_tail(new_node)
