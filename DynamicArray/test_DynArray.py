d5f5afbff1e3fc75152e50e884c1906b3568c4a2
Roman <62597624+Roman-AlexeevP@users.noreply.github.com>
2022-04-14 21:16:57 +0300

    Update test_DynArray.py

--- DynamicArray/test_DynArray.py
+++ DynamicArray/test_DynArray.py
@@ -57,6 +57,7 @@ class TestDynArray(TestCase):
         self.array.delete(0)
         self.assertEqual(self.array.capacity, 16)
         self.assertEqual(self.array.count, 0)
+        self.array.print_all()
 
     def test_delete_from_center(self):
         for i in range(1, 4):  # [1,2,3]
@@ -74,8 +75,63 @@ class TestDynArray(TestCase):
         self.assertEqual(self.array.count, 17)
         self.array.delete(16)
         self.assertEqual(self.array.count, 16)
-        self.assertEqual(self.array.capacity, 21)
+        self.assertEqual(self.array.capacity, 32)
         for _ in range(6):
             self.array.delete(1)
+        self.assertEqual(self.array[0], 1)
+        self.assertEqual(self.array[9], 16)
         self.assertEqual(self.array.count, 10)
         self.assertEqual(self.array.capacity, 16)
+
+    def test_delete_without_resize_end(self):
+        for i in range(1,10):
+            self.array.append(i)
+        self.array.delete(8)
+        self.assertEqual(self.array.count, 8)
+        self.assertEqual(self.array.capacity, 16)
+        for i in range(1,9):
+            self.assertEqual(self.array[i-1], i)
+
+    def test_delete_without_resize_begin(self):
+        for i in range(1,10):
+            self.array.append(i)
+        self.array.delete(0)
+        self.assertEqual(self.array.count, 8)
+        self.assertEqual(self.array.capacity, 16)
+        self.assertEqual(self.array[0], 2)
+        self.assertEqual(self.array[1], 3)
+        self.assertEqual(self.array[2], 4)
+        self.assertEqual(self.array[7], 9)
+
+    def test_delete_resizez_end(self):
+        for i in range(1,50):
+            self.array.append(i)
+        self.assertEqual(self.array.count, 49)
+        self.assertEqual(self.array.capacity, 64)
+        for i in range(18):
+            self.array.delete(0)
+        self.assertEqual(self.array.count, 31)
+        self.assertEqual(self.array.capacity, 42)
+        self.assertEqual(self.array[0], 19)
+        self.assertEqual(self.array[30], 49)
+        for i in range(11):
+            self.array.delete(0)
+        self.assertEqual(self.array.count, 20)
+        self.assertEqual(self.array.capacity, 28)
+        self.assertEqual(self.array[0], 30)
+        self.assertEqual(self.array[19], 49)
+        for i in range(8):
+            self.array.delete(0)
+        self.assertEqual(self.array.count,12)
+        self.assertEqual(self.array.capacity,18)
+        self.assertEqual(self.array[0], 38)
+        self.assertEqual(self.array[11], 49)
+
+    def test_delete_order(self):
+        for i in range(10):
+            self.array.append(i)
+        self.array.delete(0)
+        self.array.delete(0)
+        self.array.delete(7)
+
+
