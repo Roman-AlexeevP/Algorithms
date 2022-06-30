97782ae093e95e139fca1d33c51865b7fbc55a8d
lebelius <alexeevroma@gmail.com>
2022-06-30 17:27:08 +0300

    add recursive file printing

--- Exercises/Recursion/exercises.py
+++ Exercises/Recursion/exercises.py
@@ -1,5 +1,6 @@
 import copy
 
+# Вычисление степени
 
 def recursion_pow(base, exp):
     if exp == 0:
@@ -10,12 +11,14 @@ def recursion_pow(base, exp):
         return 1 / recursion_pow(base, -exp)
     return base * recursion_pow(base, exp - 1)
 
+# Вычисление суммы цифр числа
 
 def sum_digits(number):
     if abs(number) < 10:
         return number
     return number % 10 + sum_digits(number // 10)
 
+# длина списка
 
 def recursion_len(iterable):
     local_iterable = list(iterable)
@@ -24,6 +27,7 @@ def recursion_len(iterable):
         return 0
     return 1 + recursion_len(local_iterable)
 
+# Палиндром
 
 def recursion_palindrome(string: str) -> bool:
     length = len(string)
@@ -32,6 +36,7 @@ def recursion_palindrome(string: str) -> bool:
     begin_equal_end = string[0] == string[-1]
     return begin_equal_end and recursion_palindrome(string[1:-1])
 
+# Печать четных значений
 
 def print_even_values(iterable):
     if len(iterable) == 0:
@@ -41,6 +46,7 @@ def print_even_values(iterable):
         print(value, end=", ")
     print_even_values(iterable)
 
+# Печать значений с четным индексом
 
 def print_even_indexes(iterable):
     lentgh = len(iterable)
@@ -53,21 +59,7 @@ def print_even_indexes(iterable):
     iterable.pop(0)
     print_even_indexes(iterable)
 
-
-def recursive_max(iterable):
-    if len(iterable) == 1:
-        return iterable.pop()
-    first_value = iterable.pop()
-    second = recursive_max(iterable)
-    return first_value if first_value > second else second
-
-
-def recursive_max_2(iterable, index):
-    if index > 0:
-        first = iterable[index]
-        second = recursive_max_2(iterable, index - 1)
-        return first if first > second else second
-    return iterable[0]
+# Поиск второго максимума с повторяющимися элементами
 
 def find_max(iterable, index, first_max, second_max):
     if index == len(iterable):
@@ -87,5 +79,31 @@ def second_max(iterable):
     local_iterable = list(set(iterable))
     min_value = float("-inf")
     return find_max(local_iterable, 0, min_value, min_value)
+
+# Поиск всех файлов в каталогах и подкаталогах
+
+import os
+
+def print_all_files(path):
+    """
+    получаем список всего по пути
+    берем первый если папка то рекурсия с новым путем
+    если файл печатаем
+    """
+    if not os.path.isdir(path):
+        return None
+    recursive_print(path, os.listdir(path))
+
+
+def recursive_print(path, objects):
+    if len(objects) == 0:
+        return None
+    object_path = path + objects.pop()
+    if os.path.isdir(object_path):
+        dir_path = f"{object_path}/"
+        recursive_print(dir_path, os.listdir(dir_path))
+    if os.path.isfile(object_path):
+        print(object_path)
+    recursive_print(path, objects)
 
 
