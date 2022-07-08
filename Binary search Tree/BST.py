2c5a88a4553766c6bc5a185beac4e3a1f39b77dc
lebelius <alexeevroma@gmail.com>
2022-07-08 18:43:06 +0300

    implement binary search tree and test

---
+++ Binary search Tree/BST.py
@@ -0,0 +1,123 @@
+class BSTNode:
+
+    def __init__(self, key, val, parent):
+        self.NodeKey = key
+        self.NodeValue = val
+        self.Parent = parent
+        self.LeftChild = None
+        self.RightChild = None
+
+    def is_leaf(self):
+        return self.RightChild is None and self.LeftChild is None
+
+    def has_both_children(self):
+        return self.RightChild is not None and self.LeftChild is not None
+
+
+class BSTFind:
+
+    def __init__(self):
+        self.Node = None
+
+        self.NodeHasKey = False
+        self.ToLeft = False
+
+
+class BST:
+
+    def __init__(self, node):
+        self.Root = node
+
+    def FindNodeByKey(self, key):
+        find_result = BSTFind()
+        if self.Root is None or key is None:
+            return find_result
+        find_result.Node = self.Root
+        if self.Root.NodeKey == key:
+            find_result.NodeHasKey = True
+            return find_result
+        if key < self.Root.NodeKey:
+            if self.Root.LeftChild is None:
+                find_result.ToLeft = True
+                return find_result
+            tree = BST(self.Root.LeftChild)
+            return tree.FindNodeByKey(key)
+        if key > self.Root.NodeKey:
+            if self.Root.RightChild is None:
+                return find_result
+            tree = BST(self.Root.RightChild)
+            return tree.FindNodeByKey(key)
+
+        return find_result
+
+    def AddKeyValue(self, key, val):
+        find_result = self.FindNodeByKey(key)
+        if find_result.NodeHasKey:
+            return False
+        node = BSTNode(key=key, val=val, parent=None)
+        if find_result.Node is None:
+            self.Root = node
+            return True
+        node.Parent = find_result.Node
+        if find_result.ToLeft:
+            find_result.Node.LeftChild = node
+            return True
+        find_result.Node.RightChild = node
+        return True
+
+    def FinMinMax(self, FromNode, FindMax):
+        if FromNode is None:
+            return None
+        node = FromNode.RightChild if FindMax else FromNode.LeftChild
+        if node is None:
+            return FromNode
+        tree = BST(node)
+        return tree.FinMinMax(node, FindMax)
+
+    def DeleteNodeByKey(self, key):
+        deleted_node = self.FindNodeByKey(key).Node
+        if deleted_node is None:
+            return False
+        if deleted_node.is_leaf():
+            return self.remove_node_from_parent(deleted_node)
+        if deleted_node.RightChild is not None and deleted_node.LeftChild is not None:
+            node_succesor = self.FinMinMax(deleted_node.RightChild, FindMax=False)
+            self.copy_key_val(to_node=deleted_node, from_node=node_succesor)
+            if node_succesor.is_leaf():
+                return self.remove_node_from_parent(node_succesor)
+            self.copy_key_val(to_node=node_succesor, from_node=node_succesor.RightChild)
+            return self.remove_node_from_parent(node_succesor.RightChild)
+        new_node = None
+        if deleted_node.RightChild is not None:
+            new_node = deleted_node.RightChild
+        if deleted_node.LeftChild is not None:
+            new_node = deleted_node.LeftChild
+        self.copy_key_val(to_node=deleted_node, from_node=new_node)
+        return self.remove_node_from_parent(new_node)
+
+    def copy_key_val(self, to_node, from_node):
+        to_node.NodeKey = from_node.NodeKey
+        to_node.NodeValue = from_node.NodeValue
+
+    def remove_node_from_parent(self, node):
+        if node.Parent is None:
+            self.Root = None
+            return True
+        if node is node.Parent.LeftChild:
+            node.Parent.LeftChild = None
+        if node is node.Parent.RightChild:
+            node.Parent.RightChild = None
+        node.Parent = None
+        return True
+
+    def Count(self):
+        if self.Root is None:
+            return 0
+        count = 1
+
+        left_child_tree = BST(self.Root.LeftChild)
+        right_child_tree = BST(self.Root.RightChild)
+        count += left_child_tree.Count()
+        count += right_child_tree.Count()
+
+        return count
---
+++ Binary search Tree/test.py
@@ -0,0 +1,255 @@
+import pytest
+
+from BST import BST, BSTFind, BSTNode
+
+
+@pytest.fixture()
+def empty_find_result():
+    expected_result = BSTFind()
+    expected_result.Node = None
+    expected_result.NodeHasKey = False
+    expected_result.ToLeft = False
+    return expected_result
+
+
+@pytest.fixture()
+def tree_with_root():
+    root = BSTNode(parent=None, key=1, val=1)
+    tree = BST(root)
+    return tree, root
+
+
+def test_FindNodeByKey(tree_with_root):
+    tree, root = tree_with_root
+    expected_result = BSTFind()
+    expected_result.Node = root
+    expected_result.NodeHasKey = True
+    expected_result.ToLeft = False
+
+    result = tree.FindNodeByKey(1)
+
+    assert result.Node == expected_result.Node
+    assert result.NodeHasKey == expected_result.NodeHasKey
+    assert result.ToLeft == expected_result.ToLeft
+
+
+def test_FindNodeByKey_empty_root(empty_find_result):
+    tree = BST(node=None)
+
+    result = tree.FindNodeByKey(1)
+
+    assert result.Node == empty_find_result.Node
+    assert result.NodeHasKey == empty_find_result.NodeHasKey
+    assert result.ToLeft == empty_find_result.ToLeft
+
+
+def test_FindNodeByKey_empty_key(tree_with_root, empty_find_result):
+    tree, root = tree_with_root
+
+    result = tree.FindNodeByKey(None)
+
+    assert result.Node == empty_find_result.Node
+    assert result.NodeHasKey == empty_find_result.NodeHasKey
+    assert result.ToLeft == empty_find_result.ToLeft
+
+
+def test_FindNodeByKey_add_left(tree_with_root):
+    tree, root = tree_with_root
+
+    expected_result = BSTFind()
+    expected_result.Node = root
+    expected_result.NodeHasKey = False
+    expected_result.ToLeft = True
+
+    real_result = tree.FindNodeByKey(-1)
+
+    assert real_result.Node == expected_result.Node
+    assert real_result.NodeHasKey == expected_result.NodeHasKey
+    assert real_result.ToLeft == expected_result.ToLeft
+
+
+def test_FindNodeByKey_add_right(tree_with_root):
+    tree, root = tree_with_root
+
+    expected_result = BSTFind()
+    expected_result.Node = root
+    expected_result.NodeHasKey = False
+    expected_result.ToLeft = False
+
+    real_result = tree.FindNodeByKey(100)
+
+    assert real_result.Node == expected_result.Node
+    assert real_result.NodeHasKey == expected_result.NodeHasKey
+    assert real_result.ToLeft == expected_result.ToLeft
+
+    right_child = BSTNode(parent=root, val=2, key=2)
+    root.RightChild = right_child
+    expected_result.Node = right_child
+    real_result = tree.FindNodeByKey(100)
+
+    assert real_result.Node == expected_result.Node
+    assert real_result.NodeHasKey == expected_result.NodeHasKey
+    assert real_result.ToLeft == expected_result.ToLeft
+
+
+def test_AddKeyValue_empty_tree():
+    tree = BST(None)
+    assert tree.AddKeyValue(1, 1)
+    assert tree.Root is not None
+    find_result = tree.FindNodeByKey(1)
+
+    assert find_result.Node is not None
+    assert find_result.NodeHasKey
+
+
+def test_AddKeyValue_right(tree_with_root):
+    tree, root = tree_with_root
+
+    NEW_KEY, NEW_VALUE = 2, 2
+    assert tree.AddKeyValue(NEW_KEY, NEW_VALUE)
+    assert root.LeftChild is None
+    assert root.RightChild is not None
+    assert root.RightChild.NodeValue == NEW_VALUE
+
+    find_result = tree.FindNodeByKey(NEW_KEY)
+
+    assert find_result.Node is not None
+    assert find_result.NodeHasKey
+    assert find_result.Node.NodeValue == NEW_VALUE
+
+
+def test_AddKeyValue_left(tree_with_root):
+    tree, root = tree_with_root
+
+    NEW_KEY, NEW_VALUE = -5, -5
+    assert tree.AddKeyValue(NEW_KEY, NEW_VALUE)
+    assert root.RightChild is None
+    assert root.LeftChild is not None
+    assert root.LeftChild.NodeValue == NEW_VALUE
+
+    find_result = tree.FindNodeByKey(NEW_KEY)
+
+    assert find_result.Node is not None
+    assert find_result.NodeHasKey
+    assert find_result.Node.NodeValue == NEW_VALUE
+
+
+def test_FinMinMax_root_min_max(tree_with_root):
+    tree, root = tree_with_root
+    MIN_VALUE, MAX_VALUE = -50, 50
+    for i in range(MIN_VALUE, MAX_VALUE + 1):
+        tree.AddKeyValue(i, i)
+    assert tree.FinMinMax(root, FindMax=False).NodeKey == MIN_VALUE
+    assert tree.FinMinMax(root, FindMax=True).NodeKey == MAX_VALUE
+
+
+def test_FinMinMax_node_min_max(tree_with_root):
+    tree, root = tree_with_root
+    root.NodeKey = 10
+    root.NodeValue = 10
+
+    MIN_VALUE, MAX_VALUE = 1, 19
+    for i in range(MIN_VALUE, MAX_VALUE + 1):
+        if i == 10:
+            continue
+        tree.AddKeyValue(i, i)
+    assert tree.FinMinMax(root, FindMax=True).NodeKey == MAX_VALUE
+    assert tree.FinMinMax(root, FindMax=False).NodeKey == MIN_VALUE
+    tree.AddKeyValue(4.5, 4.5)
+    from_node = tree.FindNodeByKey(5).Node
+
+    assert tree.FinMinMax(from_node, FindMax=True).NodeKey == 9
+    assert tree.FinMinMax(from_node, FindMax=False).NodeKey == 4.5
+
+
+def test_DeleteNodeByKey_leaf_root(tree_with_root):
+    tree, root = tree_with_root
+    assert tree.DeleteNodeByKey(1)
+    assert tree.Count() == 0
+
+
+def test_DeleteNodeByKey_leaf(tree_with_root):
+    tree, root = tree_with_root
+    tree.AddKeyValue(2, 2)
+
+    assert tree.DeleteNodeByKey(2)
+    assert tree.Count() == 1
+    assert tree.Root.RightChild is None
+    assert tree.Root.LeftChild is None
+
+
+def test_DeleteNodeByKey_with_left_child(tree_with_root):
+    tree, root = tree_with_root
+    tree.AddKeyValue(3, 3)
+    tree.AddKeyValue(2, 2)
+    assert tree.DeleteNodeByKey(3)
+
+    assert tree.Count() == 2
+    assert tree.FindNodeByKey(2).Node.LeftChild is None
+    assert tree.FindNodeByKey(2).Node.RightChild is None
+
+
+def test_DeleteNodeByKey_with_right_child(tree_with_root):
+    tree, root = tree_with_root
+    tree.AddKeyValue(3, 3)
+    tree.AddKeyValue(4, 4)
+    assert tree.Count() == 3
+    assert tree.FindNodeByKey(3).Node.RightChild.NodeValue == 4
+    assert tree.FindNodeByKey(3).Node.LeftChild is None
+
+    assert tree.DeleteNodeByKey(3)
+
+    assert tree.Count() == 2
+    assert tree.Root.RightChild.NodeKey == 4
+    assert tree.FindNodeByKey(4).Node.LeftChild is None
+    assert tree.FindNodeByKey(4).Node.RightChild is None
+
+
+def test_DeleteNodeByKey_with_both_children(tree_with_root):
+    tree, root = tree_with_root
+    root.NodeKey, root.NodeValue = 10, 10
+
+    tree.AddKeyValue(15, 15)
+    tree.AddKeyValue(12, 12)
+    tree.AddKeyValue(16, 16)
+
+    assert tree.DeleteNodeByKey(15)
+    assert tree.Count() == 3
+    assert root.RightChild.NodeValue == 16
+    assert root.RightChild.LeftChild is not None and root.RightChild.LeftChild.NodeValue == 12
+
+
+def test_DeleteNodeByKey_with_both_children_not_leaf(tree_with_root):
+    tree, root = tree_with_root
+    root.NodeKey, root.NodeValue = 10, 10
+
+    tree.AddKeyValue(15, 15)
+    tree.AddKeyValue(12, 12)
+    tree.AddKeyValue(20, 20)
+    tree.AddKeyValue(16, 16)
+    tree.AddKeyValue(17, 17)
+
+    assert tree.DeleteNodeByKey(15)
+    assert tree.Count() == 5
+    assert root.RightChild.NodeValue == 16
+    assert tree.FindNodeByKey(20).Node.LeftChild.NodeValue == 17
+
+
+def test_Count(tree_with_root):
+    tree, root = tree_with_root
+
+    assert tree.Count() == 1
+
+
+def test_Count_empty():
+    tree = BST(None)
+
+    assert tree.Count() == 0
+
+
+def test_Count_with_children(tree_with_root):
+    tree, root = tree_with_root
+
+    for i in range(2, 101):
+        assert tree.AddKeyValue(i, i)
+    assert tree.Count() == 100
